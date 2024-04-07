39b0e6a28ef13c213de2c456253f7b54254dee1e
Boi San Ly <sanboily.2003@gmail.com>
2024-04-06 21:34:41 -0700

    Adding new pages

---
+++ pages/Color.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Color.module.css";
+import Footer from "@/components/Footer";
+import Navigation from "@/components/Navigation";
+
+export default function Color() {
+    return(
+        <>
+            <main>
+
+            </main>
+            <Footer/>
+        </>
+    )
+}
+
+
+
+
